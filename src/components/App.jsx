3a6c16a47beb1c0a2e26999e603c31adb9e23a3e
Andres Rodriguez Escudero <andres.rodriguez0215@gmail.com>
2020-02-02 18:03:12 -0500

    Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import  { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
+import Layout from './Layout';
+import NotFound from '../pages/NotFound';
+import Home from '../pages/Home';
 
 export default function App () {
     return (
         <BrowserRouter>
-        <Switch>
-            <Route exact path="/badges" component={Badges}/>
-            <Route exact path="/badges/new" component={BadgeNew}/>            
-        </Switch>
-
+            <Layout>
+                <Switch>
+                    <Route exact path="/badges" component={Badges}/>
+                    <Route exact path="/badges/new" component={BadgeNew}/>            
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+export default function Layout(props) {
+    return(
+        <React.Fragment>
+            <Navbar/>
+            {props.children}
+        </React.Fragment>
+    );
+}
